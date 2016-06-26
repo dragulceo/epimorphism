87212ea76c37c7280d7cb795260dda9f146590ff
Gene Shuman <geneshuman@gmail.com>
2016-06-25 21:48:41 -0700

    restate working

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-#scp -i ~/.ssh/epimorphism.pem -r html/* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism
 scp -i ~/.ssh/epimorphism.pem html/* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism
-scp -i ~/.ssh/epimorphism.pem html/lib/*lib ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism
+scp -i ~/.ssh/epimorphism.pem -r html/lib* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism
+#scp -i ~/.ssh/epimorphism.pem -r html/textures* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism

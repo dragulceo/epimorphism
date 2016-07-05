ed1153e394fe344a3cd995939fb85a4faf9184d7
Gene Shuman <geneshuman@gmail.com>
2016-07-05 13:36:52 -0700

    cleaned UI.purs

--- src/UI/KeyHandlers.purs
+++ src/UI/KeyHandlers.purs
@@ -8,7 +8,6 @@ import DOM (DOM)
 import Data.Maybe.Unsafe (fromJust)
 import Data.StrMap (insert, member, lookup)
 import Graphics.Canvas (Canvas)
-import Util (lg)
 
 -- converts key codes into command sequences
 type KeyHandler = forall eff h. STRef h UIConf -> STRef h UIST -> String -> Eff (canvas :: Canvas, dom :: DOM, st :: ST h | eff) String
@@ -17,6 +16,7 @@ keyHandler :: KeyHandler
 keyHandler ucRef usRef char = do
   uiConf <- readSTRef ucRef
 
+  --let x = lg char
   case uiConf.keySet of
     "dev"  -> devKeyHandler ucRef usRef char
     "prod" -> prodKeyHandler ucRef usRef char
@@ -28,72 +28,28 @@ devKeyHandler ucRef usRef char = do
   uiConf <- readSTRef ucRef
   uiST   <- readSTRef usRef
 
-  --let x = lg char
-  let spd = show uiConf.keyboardSwitchSpd
   case char of
     "1" -> do
-      incI uiST "main.main_body.t" "t_inner" "t_inner" "vec2" 1 spd
+      inc uiConf uiST "Sub" "main.main_body.t" "t_inner" "t_inner" "vec2" 1
     "Q" -> do
-      incI uiST "main.main_body.t" "t_inner" "t_inner" "vec2" (-1) spd
+      inc uiConf uiST "Sub" "main.main_body.t" "t_inner" "t_inner" "vec2" (-1)
     "2" -> do
-      incM uiST "disp" "post" "post_hlim" "vec4" 1 spd
+      inc uiConf uiST "Mod" "disp" "post" "post_hlim" "vec4" 1
     "W" -> do
-      incM uiST "disp" "post" "post_hlim" "vec4" (-1) spd
+      inc uiConf uiST "Mod" "disp" "post" "post_hlim" "vec4" (-1)
     "3" -> do
-      incG uiST "main.main_body.seed" "0" "basic_images" "vec4" 1 spd
+      inc uiConf uiST "Image" "main.main_body.seed" "0" "basic_images" "vec4" 1
     "E" -> do
-      incG uiST "main.main_body.seed" "0" "basic_images" "vec4" (-1) spd
---    "3" -> do
---      incM uiST "main.main_body.seed.seed0" "t" "basic_seed" "vec2" 1 spd
---    "E" -> do
---      incM uiST "main.main_body.seed.seed0" "t" "basic_seed" "vec2" (-1) spd
---    "4" -> do
---      incI uiST "main.main_body.seed.seed0.t" "t_inner" "t_inner" "vec2" 1 spd
---    "R" -> do
---      incI uiST "main.main_body.seed.seed0.t" "t_inner" "t_inner" "vec2" (-1) spd
---    "5" -> do
---      incM uiST "main.main_body" "color" "basic" "vec4" 1 spd
---    "T" -> do
---      incM uiST "main.main_body" "color" "basic" "vec4" (-1) spd
---    "0" -> do
---      incM uiST "main.main_body.seed.seed0" "seed_color" "basic" "vec4" 1 spd
---    "P" -> do
---      incM uiST "main.main_body.seed.seed0" "seed_color" "basic" "vec4" (-1) spd
---    "½" -> do
---      incM uiST "main.main_body.seed.seed1" "seed_color" "basic" "vec4" 1 spd
---    "Û" -> do
---      incM uiST "main.main_body.seed.seed1" "seed_color" "basic" "vec4" (-1) spd
-    "6" -> do
-      incS uiST "main.main_body.t" "0" "z1" "vec2" 1 spd
-    "Y" -> do
-      incS uiST "main.main_body.t" "0" "z1" "vec2" (-1) spd
+      inc uiConf uiST "Image" "main.main_body.seed" "0" "basic_images" "vec4" (-1)
     _   -> commonKeyHandler ucRef usRef char
   where
-    incG uiST bdy idn lib dim inc spd = do
-      let idn' = bdy ++ idn
-      let idx = if (member idn' uiST.incIdx) then ((fromJust $ lookup idn' uiST.incIdx) + inc) else 0
-      let dt = insert idn' idx uiST.incIdx
-      modifySTRef usRef (\s -> s {incIdx = dt})
-      return $ "scr incImage " ++  bdy ++ " sub:" ++ idn ++ " lib:" ++ lib ++ " dim:" ++ dim ++ " idx:" ++ (show idx) ++ " spd:" ++ spd
-    incS uiST bdy idn lib dim inc spd = do
-      let idn' = bdy ++ idn
-      let idx = if (member idn' uiST.incIdx) then ((fromJust $ lookup idn' uiST.incIdx) + inc) else 0
-      let dt = insert idn' idx uiST.incIdx
-      modifySTRef usRef (\s -> s {incIdx = dt})
-      return $ "scr incScript " ++  bdy ++ " sub:" ++ idn ++ " lib:" ++ lib ++ " dim:" ++ dim ++ " idx:" ++ (show idx) ++ " spd:" ++ spd
-    incM uiST bdy idn lib dim inc spd = do
-      let idn' = bdy ++ idn
-      let idx = if (member idn' uiST.incIdx) then ((fromJust $ lookup idn' uiST.incIdx) + inc) else 0
-      let dt = insert idn' idx uiST.incIdx
-      modifySTRef usRef (\s -> s {incIdx = dt})
-      return $ "scr incMod " ++  bdy ++ " sub:" ++ idn ++ " lib:" ++ lib ++ " dim:" ++ dim ++ " idx:" ++ (show idx) ++ " spd:" ++ spd
-    incI uiST addr sub lib dim inc spd = do
+    inc uiConf uiST typ addr sub lib dim ofs = do
       let idn' = addr ++ sub
-      let idx = if (member idn' uiST.incIdx) then ((fromJust $ lookup idn' uiST.incIdx) + inc) else 0
+      let idx = if (member idn' uiST.incIdx) then ((fromJust $ lookup idn' uiST.incIdx) + ofs) else 0
       let dt = insert idn' idx uiST.incIdx
       modifySTRef usRef (\s -> s {incIdx = dt})
-      return $ "scr incSub " ++  addr ++ " sub:" ++ sub ++ " lib:" ++ lib ++ " dim:" ++ dim ++ " idx:" ++ (show idx) ++ " spd:" ++ spd
-
+      let spd = show uiConf.keyboardSwitchSpd
+      return $ "scr inc" ++ typ ++ " " ++  addr ++ " sub:" ++ sub ++ " lib:" ++ lib ++ " dim:" ++ dim ++ " idx:" ++ (show idx) ++ " spd:" ++ spd
 
 prodKeyHandler :: KeyHandler
 prodKeyHandler ucRef usRef char = do

0c02b176cf91743e500f080dfd8157706d943798
Gene Shuman <geneshuman@gmail.com>
2016-09-16 13:40:13 -0700

    updated about info

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 scp -i ~/.ssh/epimorphism.pem html/* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism
 scp -i ~/.ssh/epimorphism.pem -r html/javascript* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism
-scp -i ~/.ssh/epimorphism.pem html/lib/* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism
+scp -i ~/.ssh/epimorphism.pem html/lib/* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism/lib
 #scp -i ~/.ssh/epimorphism.pem -r html/images* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism
 #scp -i ~/.ssh/epimorphism.pem -r html/textures* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism/textures

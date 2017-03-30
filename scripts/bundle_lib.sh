a2005dcce136312f9400edc265e0d088d186ba9c
gene <gene@valimail.com>
2017-03-30 02:13:50 -0700

    fix deploy, watch, etc

--- scripts/bundle_lib.sh
+++ scripts/bundle_lib.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Bundling Libraries"
-for each in html/lib/components/*.slib; do cat $each; printf "\n\n\n"; done > html/lib/components.slib
-for each in html/lib/indexes/*.slib; do cat $each; printf "\n\n\n"; done > html/lib/indexes.slib
-for each in html/lib/modules/*.lib; do cat $each; printf "\n\n\n"; done > html/lib/modules.lib
-printf "\n\n\n" >> html/lib/modules.lib
-for each in html/lib/modules/save/*.lib; do cat $each; printf "\n\n\n"; done >> html/lib/modules.lib
+for each in lib/components/*.slib; do cat $each; printf "\n\n\n"; done > lib/components.slib
+for each in lib/indexes/*.slib; do cat $each; printf "\n\n\n"; done > lib/indexes.slib
+for each in lib/modules/*.lib; do cat $each; printf "\n\n\n"; done > lib/modules.lib
+printf "\n\n\n" >> lib/modules.lib
+for each in lib/modules/save/*.lib; do cat $each; printf "\n\n\n"; done >> lib/modules.lib
--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 killall fswatch
-fswatch -o html/lib/components/*.slib  | xargs -n1 ./scripts/bundle_lib.sh &
-fswatch -o html/lib/indexes/*.slib     | xargs -n1 ./scripts/bundle_lib.sh &
-fswatch -o html/lib/modules/*.lib      | xargs -n1 ./scripts/bundle_lib.sh &
-fswatch -o html/lib/modules/save/*.lib | xargs -n1 ./scripts/bundle_lib.sh &
+fswatch -o lib/components/*.slib  | xargs -n1 ./scripts/bundle_lib.sh &
+fswatch -o lib/indexes/*.slib     | xargs -n1 ./scripts/bundle_lib.sh &
+fswatch -o lib/modules/*.lib      | xargs -n1 ./scripts/bundle_lib.sh &
+fswatch -o lib/modules/save/*.lib | xargs -n1 ./scripts/bundle_lib.sh &
 pulp --watch --before 'clear;./scripts/bundle_lib.sh' --then './scripts/finish.sh' browserify --standalone Main --to html/index.tmp

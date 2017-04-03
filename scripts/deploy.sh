d830acbc6c7f4fbcba67d98c0a7b9cce26e276c9
gene <gene@valimail.com>
2017-04-03 13:09:07 -0700

    minor changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,5 @@ scp -i ~/.ssh/epimorphism.pem -r html/javascript* ubuntu@52.40.74.208:/home/ubun
 scp -i ~/.ssh/epimorphism.pem -r html/stylesheets* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism
 scp -i ~/.ssh/epimorphism.pem lib/* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism/lib
 scp -i ~/.ssh/epimorphism.pem -r html/misc* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism
-#scp -i ~/.ssh/epimorphism.pem -r html/images* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism
+scp -i ~/.ssh/epimorphism.pem -r html/images* ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism
 #scp -i ~/.ssh/epimorphism.pem -r html/textures ubuntu@52.40.74.208:/home/ubuntu/www/epimorphism

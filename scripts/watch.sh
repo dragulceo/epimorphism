d1a86200c88a6a18fb9bc1e6e404b0bfe19f6af4
gene <gene@valimail.com>
2017-04-08 19:33:25 -0700

    cleaned up lib

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -3,7 +3,7 @@
 killall fswatch
 fswatch -o lib/core/*.lib  | xargs -n1 ./scripts/bundle_lib.sh &
 fswatch -o lib/core/**/*.lib  | xargs -n1 ./scripts/bundle_lib.sh &
+fswatch -o lib/user/*.lib  | xargs -n1 ./scripts/bundle_lib.sh &
+fswatch -o lib/user/**/*.lib  | xargs -n1 ./scripts/bundle_lib.sh &
 fswatch -o lib/sections/*.slib     | xargs -n1 ./scripts/bundle_lib.sh &
-#fswatch -o lib/modules/*.lib      | xargs -n1 ./scripts/bundle_lib.sh &
-#fswatch -o lib/modules/save/*.lib | xargs -n1 ./scripts/bundle_lib.sh &
 pulp --watch --before 'clear;./scripts/bundle_lib.sh' --then './scripts/finish.sh' browserify --standalone Main --to html/index.tmp

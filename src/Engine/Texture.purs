56b9003b007a0a6bddfd0b4676e50bdb2bbf52b7
Gene Shuman <geneshuman@gmail.com>
2017-09-09 17:46:00 -0700

    updates for langton 10 year party

--- src/Engine/Texture.purs
+++ src/Engine/Texture.purs
@@ -46,7 +46,7 @@ initTexFb dim = do
   ctx <- ask
   tex <- newTex
   fb <- createFramebuffer
-  liftEff $ GL.texImage2D_ ctx GLE.texture2d 0 GLE.rgba dim dim 0 GLE.rgba GLE.float (unsafeNull :: GLT.ArrayBufferView)
+  liftEff $ GL.texImage2D_ ctx GLE.texture2d 0 GLE.rgba dim dim 0 GLE.rgba GLE.unsignedByte (unsafeNull :: GLT.ArrayBufferView)
   liftEff $ GL.bindFramebuffer ctx GLE.framebuffer fb
   liftEff $ GL.framebufferTexture2D ctx GLE.framebuffer GLE.colorAttachment0 GLE.texture2d tex 0
 

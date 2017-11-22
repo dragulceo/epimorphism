0a8bfa4384cfcdcf0559343286f12b395c10e63f
Gene Shuman <geneshuman@gmail.com>
2017-11-21 19:32:07 -0800

    misc updates

--- src/Command.purs
+++ src/Command.purs
@@ -70,6 +70,8 @@ command usRef esRef ssRef lib msg = handleError do
 
             pure unit
           "killPaths" -> do
+            -- not working??
+
             let sch = buildSearch ["live"] [] []
             modules <- searchLib lib sch
 
